c5ea14e36585e57a8ce3c6702f09dfc797e0074e
Michael Jackson <mike.jackson@bluequartz.net>
2022-03-22 15:34:28 -0400

    Ready for 1.4.4 release

--- Conda/setup.py
+++ Conda/setup.py
@@ -5,7 +5,7 @@ ext = get_config_var('EXT_SUFFIX')
 
 setup(
     name='dream3d',
-    version='1.4.3',
+    version='1.4.4',
     packages=find_packages(),
     package_data={'dream3d': [f'*{ext}', '*.pyi', 'py.typed']},
     include_package_data=True,
